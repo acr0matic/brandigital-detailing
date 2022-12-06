7e8979336ec36c6dd03ffa648277a829812379b7
acr0matic <work.acr0matic@gmail.com>
2022-12-06 17:50:15 +0300

    feat: логика для хедера

--- src/scripts/base.js
+++ src/scripts/base.js
@@ -2,3 +2,30 @@ const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
 
 const isMobile = window.matchMedia('(max-width: 576px)').matches;
 const isTablet = window.matchMedia('(max-width: 991px)').matches;
+
+const modalParams = {
+  awaitCloseAnimation: true,
+  disableFocus: true,
+}
+
+const scrollParams = {
+  speed: 500,
+  speedAsDuration: true,
+  offset: -90,
+  header: '#header',
+}
+
+let currentModal = null;
+
+if (isTablet) scrollParams.offset = -30;
+if (isMobile) scrollParams.offset = -15;
+
+const StyleСlass = {
+  'header': {
+    'background': 'header--background',
+  },
+
+  'mobile': {
+    'open': 'mobile-menu--open',
+  },
+}
---
+++ src/scripts/header.js
@@ -0,0 +1,42 @@
+const header = document.getElementById('header');
+
+if (header) {
+
+  let isReach = false;
+  let isOpen = false;
+
+  SetHeight(header);
+  window.addEventListener('resize', () => SetHeight(header));
+
+  function SetHeight(target) {
+    const height = target.offsetHeight;
+    document.documentElement.style.setProperty('--height', height + 'px');
+  }
+
+  window.addEventListener('scroll', () => {
+    isReach = window.scrollY > 0;
+
+    if (isReach || isOpen) header.classList.add(StyleСlass.header.background);
+    else header.classList.remove(StyleСlass.header.background);
+  });
+
+
+  // const mobile = document.querySelector('#mobile-menu');
+  // const mobileNav = mobile.querySelectorAll('.nav__link');
+  // const burger = header.querySelector('.header__action .hamburger');
+
+  // burger.addEventListener('click', () => Menu());
+
+  // mobileNav.forEach(link => {
+  //   link.addEventListener('click', () => Menu());
+  // });
+
+  // function Menu() {
+  //   isOpen = !isOpen;
+
+  //   if (!isReach) header.classList.toggle(StyleСlass.header.background);
+
+  //   mobile.classList.toggle(StyleСlass.mobile.open);
+  //   burger.classList.toggle('is-active')
+  // }
+}
